b204a02d56725323ebc7f72d3e40f3151508415c
Andrew Washburn <iamawash1@gmail.com>
2018-12-09 14:24:41 -0500

    Added some basic DOM manipulation with javascript

---
+++ dom.js
@@ -0,0 +1,45 @@
+//initialize some variables
+const container = document.querySelector('#container');
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorius text-content';
+
+//add redP to the document
+const redP = document.createElement('p');
+redP.classList.add('redP');
+redP.style.color = 'red';
+redP.textContent = "Hey I'm red";
+
+//add blueH to the document
+const blueH = document.createElement('h3');
+blueH.classList.add('blueH');
+blueH.style.color = 'blue';
+blueH.textContent = "I'm a blue h3!"
+
+//add sDiv to the document
+const sDiv = document.createElement('div');
+sDiv.classList.add('sDiv');
+sDiv.style.border = '1px solid black';
+sDiv.style.backgroundColor = 'pink';
+
+//add dH1 to the sDiv
+const dH1 = document.createElement('h1');
+dH1.classList.add('dH1');
+dH1.textContent = "I'm in a Div"
+sDiv.appendChild(dH1);
+
+//add p to the sDiv
+const p = document.createElement('p');
+p.classList.add('p');
+p.textContent = 'ME TOO!';
+sDiv.appendChild(p)
+
+
+container.appendChild(content);
+container.appendChild(redP);
+container.appendChild(blueH);
+container.appendChild(sDiv);
+
+
+var btn = document.querySelector('#btn');
+btn.onclick = () => alert("You clicked the button");
